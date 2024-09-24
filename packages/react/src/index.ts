ce5693576657ba99fdca168ec028957e6df17b6e
Carefree-happy <2507057523@qq.com>
2024-09-24 18:17:49 +0800

    feat: 2-1实现JSX

---
+++ packages/react/src/index.ts
@@ -0,0 +1,6 @@
+import { jsxDev } from './jsx';
+
+export default {
+	vaersion: '0.0.0',
+	createElement: jsxDev
+};
---
+++ packages/react/src/jsx.ts
@@ -0,0 +1,65 @@
+import {
+	ElementType,
+	Key,
+	Props,
+	Ref,
+	Type,
+	ReactElementType
+} from 'shared/ElementType';
+import { REACT_ELEMENT_TYPE } from './../../shared/ReactSymbol';
+
+const ReactElement = function (
+	type: Type,
+	key: Key,
+	ref: Ref,
+	props: Props
+): ReactElementType {
+	const element = {
+		$$typeof: REACT_ELEMENT_TYPE,
+		type,
+		key,
+		ref,
+		props,
+		__mark: 'sun'
+	};
+
+	return element;
+};
+
+export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
+
+	for (const prop in props) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+		if (Object.hasOwn(config, prop)) {
+			props[prop] = val;
+		}
+	}
+
+	const maybeChildrenLength = maybeChildren.length;
+	if (maybeChildrenLength) {
+		if (maybeChildrenLength === 1) {
+			props.children = maybeChildren[0];
+		} else {
+			props.children = maybeChildren;
+		}
+	}
+
+	return ReactElement(type, key, ref, props);
+};
+
+export const jsxDev = jsx;
