4dc3c33ac7c4e487cfe23b74765df3ebc049a992
Carefree-happy <2507057523@qq.com>
2024-09-27 15:31:12 +0800

    feat: 6-2实现Mutation子阶段

---
+++ packages/react-reconciler/src/commitWork.ts
@@ -0,0 +1,87 @@
+import { appendChildToContainer, Container } from 'hostConfig';
+import { FiberNode, FiberRootNode } from './fiber';
+import { MutationMask, NoFlags, Placement } from './fiberFlags';
+import { HostComponent, HostRoot, HostText } from './workTags';
+
+let nextEffect: FiberNode | null = null;
+export const commitMutationEffects = (finishedWork: FiberNode) => {
+	nextEffect = finishedWork;
+
+	while (nextEffect !== null) {
+		const child: FiberNode | null = nextEffect.child;
+
+		if (
+			(nextEffect.subtreeFlags & MutationMask) !== NoFlags &&
+			child !== null
+		) {
+			nextEffect = child;
+		} else {
+			up: while (nextEffect !== null) {
+				commitMutationEffectsOnFiber(nextEffect);
+				const sibling: FiberNode | null = nextEffect.sibling;
+
+				if (sibling !== null) {
+					nextEffect = sibling;
+					break up;
+				}
+				nextEffect = nextEffect.return;
+			}
+		}
+	}
+};
+
+const commitMutationEffectsOnFiber = (finishedWork: FiberNode) => {
+	const flags = finishedWork.flags;
+
+	if ((flags & Placement) !== NoFlags) {
+		commitPlacement(finishedWork);
+		finishedWork.flags &= ~Placement;
+	}
+};
+
+const commitPlacement = (finishedWork: FiberNode) => {
+	if (__DEV__) {
+		console.log('执行commitPlacement操作', finishedWork);
+	}
+	const hostParent = getHostParent(finishedWork);
+	appendPlacementNodeIntoContainer(finishedWork, hostParent);
+};
+
+function getHostParent(fiber: FiberNode) {
+	let parent = fiber.return;
+
+	while (parent) {
+		const parentTag = parent.tag;
+		if (parentTag === HostComponent) {
+			return parent.stateNode as Container;
+		}
+		if (parentTag === HostRoot) {
+			return (parent.stateNode as FiberRootNode).container;
+		}
+		parent = parent.return;
+	}
+	if (__DEV__) {
+		console.log('未找到host parent', parent);
+	}
+}
+
+function appendPlacementNodeIntoContainer(
+	finisherWork: FiberNode,
+	hostParent: Container
+) {
+	if (finisherWork.tag === HostComponent || finisherWork.tag === HostText) {
+		appendChildToContainer(finisherWork.stateNode, hostParent);
+		return;
+	}
+
+	const child = finisherWork.child;
+	if (child !== null) {
+		appendPlacementNodeIntoContainer(child, hostParent);
+		let sibling = child.sibling;
+
+		while (sibling !== null) {
+			appendPlacementNodeIntoContainer(sibling, hostParent);
+			sibling = sibling.sibling;
+		}
+	}
+}
