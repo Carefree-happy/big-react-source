09de97c623bcdbfd6319fe5f560ccdcb917296b9
Carefree-happy <2507057523@qq.com>
2024-10-16 19:40:12 +0800

    feat: 11实现事件系统

--- demos/test-fc/main.tsx
+++ demos/test-fc/main.tsx
@@ -1,16 +1,59 @@
 import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-
 function App() {
-	const [num, setNum] = useState(1);
+	const [num, setNum] = useState(3);
+	// const [num1, setNum1] = useState(3);
+	// const [num2, setNum2] = useState(3);
+	// if (num === 4) {
+	//   const [num3, setNum4] = useState(3);
+	// }
 	window.setNum = setNum;
-	return num === 3 ? <div>{num}</div> : <Child />;
+	return (
+		<div
+			onClick={() => {
+				console.log('container click');
+			}}
+			onClickCapture={() => {
+				console.log('container onClickCapture');
+			}}
+		>
+			<div
+				onClick={() => {
+					console.log('div click');
+				}}
+				onClickCapture={() => {
+					console.log('div onClickCapture');
+				}}
+			>
+				<p
+					onClickCapture={() => {
+						console.log('p onClickCapture');
+					}}
+					onClick={() => {
+						console.log('p-click');
+						setNum(num + 1);
+					}}
+				>
+					{num}
+				</p>
+			</div>
+		</div>
+	);
 }
+
+// function App() {
+//   const [num, setNum] = useState(100);
+//   window.setNum = setNum;
+//   return (
+//     <div>
+//       <span>{num}</span>
+//     </div>
+//   );
+// }
 
 function Child() {
-	return <span>big-react</span>;
+	return <div>Hello World</div>;
 }
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<App />
 );
--- packages/react-reconciler/src/completeWork.ts
+++ packages/react-reconciler/src/completeWork.ts
@@ -12,6 +12,7 @@ import {
 	HostText
 } from './workTags';
 import { NoFlags, Update } from './fiberFlags';
+import { updateFiberProps } from 'react-dom/src/SyntheticEvent';
 function markUpdate(fiber: FiberNode) {
 	fiber.flags |= Update;
 }
@@ -24,8 +25,9 @@ export const completeWork = (wip: FiberNode) => {
 		case HostComponent:
 			if (current !== null && current.stateNode) {
 				// update
+				updateFiberProps(wip.stateNode, newProps);
 			} else {
-				const instance = createInstance(wip.type);
+				const instance = createInstance(wip.type, newProps);
 				appendAllChildren(instance, wip);
 				wip.stateNode = instance;
 			}
