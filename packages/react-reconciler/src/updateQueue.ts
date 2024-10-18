a796e7646ad63fb4da19aa87ea456925350dfda7
Carefree-happy <2507057523@qq.com>
2024-10-18 16:43:01 +0800

    feat: 14-1批处理的概念

--- packages/react-reconciler/src/updateQueue.ts
+++ packages/react-reconciler/src/updateQueue.ts
@@ -3,6 +3,7 @@ import { Dispatch } from 'react/src/currentDispatcher';
 
 export interface Update<State> {
 	action: Action<State>;
+	next: Update<any> | null;
 }
 
 export interface UpdateQueue<State> {
@@ -14,7 +15,8 @@ export interface UpdateQueue<State> {
 
 export const createUpdate = <State>(action: Action<State>): Update<State> => {
 	return {
-		action
+		action,
+		next: null
 	};
 };
 
@@ -31,7 +33,20 @@ export const enqueueUpdate = <State>(
 	updateQueue: UpdateQueue<State>,
 	update: Update<State>
 ) => {
+	// 支持存放多个 update
+	const pending = updateQueue.shared.pending;
+	if (pending === null) {
+		// pending -> a -> a a和自己形成环状链表
+		update.next = update;
+	} else {
+		// pending -> b -> a -> b
+		// pending -> c -> a -> b -> c
+		update.next = pending.next;
+		pending.next = update;
+	}
 	updateQueue.shared.pending = update;
+	// pending 始终指向最后一个 update
+	// pending.next 就能拿到第一个 update
 };
 
 export const processUpdateQueue = <State>(
